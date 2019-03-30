0e0eb55479746a9ae58758fda3c8041ed6423eb7
Moshe Malawach <moshe.malawach@protonmail.com>
2019-03-30 15:02:06 +0100

    move create_post to posts.js

--- src/api/create.js
+++ src/api/create.js
@@ -32,38 +32,6 @@ export async function ipfs_push_file (fileobject,
   }
 }
 
-export async function create_post (address, post_type, content,
-                                   {api_server = DEFAULT_SERVER,
-                                    ref = null, misc_content = null} = {}) {
-  let post_content = {
-    'type': post_type,
-    'address': address,
-    'content': {
-      'body': content
-    },
-    'time': Date.now() / 1000
-  }
-
-  if (ref !== null) {
-    post_content.ref = ref
-  }
-  if (misc_content !== null) {
-    Object.assign(post_content.content, misc_content);
-  }
-
-  let hash = await ipfs_push(post_content, {'api_server':api_server})
-
-  let message = {
-    'item_hash': hash,
-    'chain': 'NULS',
-    'channel': 'blogs',
-    'sender': address,
-    'type': 'POST',
-    'time': Date.now() / 1000
-  }
-  return message
-}
-
 export async function broadcast (message,
                                  {api_server = DEFAULT_SERVER} = {}) {
   let response = await axios.post(`${api_server}/api/v0/ipfs/pubsub/pub`, {
