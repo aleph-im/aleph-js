b96fd43fbd9f22d4404f58905e65cc2b140e3907
moshe malawach <moshe.malawach@protonmail.com>
2019-04-01 15:08:55 +0200

    better functions name for aggregates

--- src/api/aggregates.js
+++ src/api/aggregates.js
@@ -2,16 +2,20 @@ import axios from 'axios'
 import {ipfs_push} from './create'
 import {DEFAULT_SERVER} from './base'
 
-export async function fetch_profile(address, {api_server = DEFAULT_SERVER} = {}) {
-  let response = await axios.get(`${api_server}/api/v0/aggregates/${address}.json?keys=profile`)
-  if ((response.data.data !== undefined) && (response.data.data.profile !== undefined))
+export async function fetch(address, key, {api_server = DEFAULT_SERVER} = {}) {
+  let response = await axios.get(`${api_server}/api/v0/aggregates/${address}.json?keys=${key}`)
+  if ((response.data.data !== undefined) && (response.data.data[key] !== undefined))
   {
-    return response.data.data.profile
+    return response.data.data[key]
   } else
     return null
 }
 
-export async function submit_aggregate(address, key, content, {api_server = DEFAULT_SERVER} = {}) {
+export async function fetch_profile(address, {api_server = DEFAULT_SERVER} = {}) {
+  return await fetch(address, 'profile', {'api_server': api_server})
+}
+
+export async function submit( address, key, content, {chain='NULS' , api_server = DEFAULT_SERVER} = {}) {
   let post_content = {
     'address': address,
     'key': key,
@@ -22,7 +26,7 @@ export async function submit_aggregate(address, key, content, {api_server = DEFA
   let hash = await ipfs_push(post_content, {api_server: api_server})
   let message = {
     'item_hash': hash,
-    'chain': 'NULS',
+    'chain': chain,
     'channel': 'blogs',
     'sender': address,
     'type': 'AGGREGATE',
