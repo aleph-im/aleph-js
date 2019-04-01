037010be47a40941200ed723daa8a27d445b2c6a
moshe malawach <moshe.malawach@protonmail.com>
2019-04-01 14:32:43 +0200

    fixing build system and dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,24 @@
 // Adapted from https://github.com/rollup/rollup-starter-lib/
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 //import replace from 'rollup-plugin-replace'
 import pkg from './package.json';
-console.log(process.env.NODE_ENV);
+
 export default [
 	// browser-friendly UMD build
 	{
 		input: 'src/index.js',
 		output:{
-			name: 'nuls-js',
+			name: pkg.name,
       file: pkg.browser,
       format: 'umd'
     },
-		moduleName: pkg.name,
 		plugins: [
 			resolve(),
       // replace(),
-      commonjs()
+	    commonjs(),
+			json()
 		]
 	},
 
@@ -28,8 +29,6 @@ export default [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
 		],
-		plugins: [
-      // replace()
-    ]
+		plugins: [ ]
 	}
 ];
