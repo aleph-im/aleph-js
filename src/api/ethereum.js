52b4b287d5a1db7ea49061688a2f437fe862fe51
moshe malawach <moshe.malawach@protonmail.com>
2019-04-24 17:33:37 +0200

    using w3 personal sign instead

--- src/api/ethereum.js
+++ src/api/ethereum.js
@@ -7,12 +7,13 @@ export async function sign(w3, address, message) {
   console.log(1)
   let buffer = get_verification_buffer(message)
   console.log(2)
+  let signed = await w3.eth.personal.sign(buffer.toString(), address, '')
   //let signed = await ethereum.eth.sign(address, buffer.toString('UTF-8'))
-  let signed = w3.currentProvider.sendAsync({
-    id: 1,
-    method: 'personal_sign',
-    params: [address, buffer.toString()] 
-  })
+  // let signed = w3.currentProvider.sendAsync({
+  //   id: 1,
+  //   method: 'personal_sign',
+  //   params: [address, buffer.toString()]
+  // })
   console.log(3)
   console.log(signed)
   message.signature = signed
