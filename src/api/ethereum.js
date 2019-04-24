92ab524d66c5d6a7b7c06666d8c1b445db18a502
moshe malawach <moshe.malawach@protonmail.com>
2019-04-24 17:23:36 +0200

    ethereum api wip (do not use yet)

---
+++ src/api/ethereum.js
@@ -0,0 +1,20 @@
+function get_verification_buffer(message) {
+  // Returns a serialized string to verify the message integrity
+  return Buffer.from(`${message.chain}\n${message.sender}\n${message.type}\n${message.item_hash}`)
+}
+
+export async function sign(w3, address, message) {
+  console.log(1)
+  let buffer = get_verification_buffer(message)
+  console.log(2)
+  //let signed = await ethereum.eth.sign(address, buffer.toString('UTF-8'))
+  let signed = w3.currentProvider.sendAsync({
+    id: 1,
+    method: 'personal_sign',
+    params: [address, buffer.toString()] 
+  })
+  console.log(3)
+  console.log(signed)
+  message.signature = signed
+  return message
+}
