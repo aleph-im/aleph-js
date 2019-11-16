4ba09d7f35aefa757f100e8af598aa3c173cfce4
Moshe Malawach <moshe.malawach@protonmail.com>
2019-11-16 13:06:08 +0100

    better api for aggregates

--- src/api/aggregates.js
+++ src/api/aggregates.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 import {ipfs_push} from './create'
 import {DEFAULT_SERVER} from './base'
+import * as nuls2 from './nuls2'
+import {broadcast} from './create'
+const shajs = require('sha.js')
 
 export async function fetch_one(address, key, {api_server = DEFAULT_SERVER} = {}) {
   let response = await axios.get(`${api_server}/api/v0/aggregates/${address}.json?keys=${key}`)
@@ -33,23 +36,43 @@ export async function fetch_profile(address, {api_server = DEFAULT_SERVER} = {})
 }
 
 export async function submit(address, key, content,
-                             {chain='NULS', channel=null,
-                              api_server = DEFAULT_SERVER} = {}) {
-  let post_content = {
+                             {chain=null, channel=null,
+                              api_server = DEFAULT_SERVER,
+                              inline = true, account = null} = {}) {
+                              
+  let aggregate_content = {
     'address': address,
     'key': key,
     'content': content,
     'time': Date.now() / 1000
   }
-
-  let hash = await ipfs_push(post_content, {api_server: api_server})
   let message = {
-    'item_hash': hash,
     'chain': chain,
     'channel': channel,
     'sender': address,
     'type': 'AGGREGATE',
     'time': Date.now() / 1000
   }
+  if (inline) {
+    let serialized = JSON.stringify(aggregate_content)
+
+    message['item_content'] = serialized
+    message['item_hash'] = new shajs.sha256().update(serialized).digest('hex')
+  } else {
+    let hash = await ipfs_push(post_content, {api_server: api_server})
+    message['item_hash'] = hash
+  }
+
+  if(account) {
+    if (!message['chain']) {
+      message['chain'] = account['type']
+    }
+    if (account.type === 'NULS2') {
+      nuls2.sign(account.private_key, message)
+    } else
+      return message // can't sign, so can't broadcast
+    await broadcast(message, {'api_server': api_server})
+  }
+
   return message
 }
