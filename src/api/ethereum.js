02cff721fa05c0ecf65c8f4aa1cf746da1ae2322
Moshe Malawach <moshe.malawach@protonmail.com>
2020-03-17 18:04:46 +0100

    fix ethereum private key import

--- src/api/ethereum.js
+++ src/api/ethereum.js
@@ -64,7 +64,7 @@ export async function import_account({
   if (mnemonics) {
     wallet = ethers.Wallet.fromMnemonic(mnemonics, path)
   } else if (private_key !== null) {
-    wallet = ethers.Wallet(private_key)
+    wallet = new ethers.Wallet(private_key)
   }
   return await _from_wallet(wallet)
 }
