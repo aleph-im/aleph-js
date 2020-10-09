88b1e1f248bb69903e6df98dfd7e7440d370b977
Moshe Malawach <moshe.malawach@protonmail.com>
2020-10-09 13:08:12 +0200

    moving rollup to new versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // Adapted from https://github.com/rollup/rollup-starter-lib/
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 //import replace from 'rollup-plugin-replace'
 import pkg from './package.json';
 
@@ -11,15 +11,15 @@ export default [
 		input: 'src/index.js',
 		output:{
 			name: pkg.name,
-      file: pkg.browser,
-      format: 'umd'
-    },
+		file: pkg.browser,
+		format: 'umd'
+		},
 		plugins: [
 			resolve({
 				browser: true
 			}),
       // replace(),
-	    commonjs(),
+	    	commonjs(),
 			json()
 		]
 	},
@@ -28,6 +28,7 @@ export default [
 		input: 'src/index.js',
 		external: [
 			'ms',
+			'@cosmjs/launchpad',
 			'@cityofzion/neon-core',
 			'@cityofzion/neon-js',
 			'axios'
@@ -36,6 +37,7 @@ export default [
 			{ file: pkg.main, format: 'cjs' },
 			{ file: pkg.module, format: 'es' }
 		],
-		plugins: [ ]
+		plugins: [ 
+		]
 	}
 ];
