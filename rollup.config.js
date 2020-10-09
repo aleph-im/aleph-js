a026f8801b068c9f260341427912466597693430
Moshe Malawach <moshe.malawach@protonmail.com>
2020-10-09 16:43:43 +0200

    moving the umd away to test

--- rollup.config.js
+++ rollup.config.js
@@ -6,38 +6,51 @@ import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 export default [
-	// browser-friendly UMD build
-	{
-		input: 'src/index.js',
-		output:{
-			name: pkg.name,
-		file: pkg.browser,
-		format: 'umd'
-		},
-		plugins: [
-			resolve({
-				browser: true
-			}),
-      // replace(),
-	    	commonjs(),
-			json()
-		]
-	},
+  // browser-friendly UMD build
+  // {
+  //   input: 'src/index.js',
+  //   external: [
+  //     'ms',
+  //     'bip39',
+  //     'eciesjs',
+  //     '@cosmjs/launchpad',
+  //     '@cityofzion/neon-core',
+  //     '@cityofzion/neon-js',
+  //     '@polkadot/util-crypto',
+  //     '@polkadot/keyring',
+  //     'axios'
+  //   ],
+  //   output: {
+  //     name: pkg.name,
+  //     file: pkg.browser,
+  //     format: 'umd'
+  //   },
+  //   plugins: [
+  //     resolve({
+  //       browser: true
+  //     }),
+  //     // replace(),
+  //     commonjs(),
+  //     json()
+  //   ]
+  // },
 
-	{
-		input: 'src/index.js',
-		external: [
-			'ms',
-			'@cosmjs/launchpad',
-			'@cityofzion/neon-core',
-			'@cityofzion/neon-js',
-			'axios'
-		],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		],
-		plugins: [ 
-		]
-	}
+  {
+    input: 'src/index.js',
+    external: [
+      'ms',
+      '@cosmostation/cosmosjs',
+      '@cityofzion/neon-core',
+      '@cityofzion/neon-js',
+      '@polkadot/util-crypto',
+      '@polkadot/keyring',
+      'axios'
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+    ]
+  }
 ];
